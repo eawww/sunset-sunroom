605e50de0cd4bb5c2622aeaac331821d4e7a648c
eric <eawww0@gmail.com>
2021-02-27 20:55:59 -0600

    Hardcode colors for various states

--- colors.js
+++ colors.js
@@ -1,19 +1,161 @@
+// module.exports = {
+//   DAYTIME: [
+//     {
+//       bri: 254,
+//       hue: 41442,
+//       sat: 75,
+//     }
+//   ],
+//   GOLDEN_HR: [
+//     {
+//       bri: 240,
+//       hue: 39392,
+//       sat: 13,
+//     },
+//   ],
+//   SUNSET: [
+//     {
+//       bri: 103,
+// 			hue: 3951,
+// 			sat: 176,
+//     },
+//     {
+//       bri: 103,
+// 			hue: 9399,
+// 			sat: 246,
+//     },
+//     {
+//       bri: 103,
+// 			hue: 5707,
+// 			sat: 254,
+//     },
+//   ],
+//   DUSK: [
+//     {
+//       bri: 103,
+// 			hue: 1500,
+// 			sat: 245,
+//     },
+//     {
+//       bri: 103,
+// 			hue: 64224,
+// 			sat: 191,
+//     },
+//     {
+//       bri: 103,
+// 			hue: 5707,
+// 			sat: 254,
+//     }
+//   ],
+//   NAUTICAL_DUSK: [],
+//   NIGHT: [],
+// }
+
 module.exports = {
-  DAYTIME: [
+  nadir: [
     {
-      bri: 254,
-      hue: 41442,
-      sat: 75,
+      bri: 94,
+			hue: 45915,
+			sat: 254,
     }
   ],
-  GOLDEN_HR: [
+  nightEnd: [
     {
-      bri: 240,
-      hue: 39392,
-      sat: 13,
+      bri: 180,
+			hue: 46764,
+			sat: 194,
+    }
+  ],
+  nauticalDawn: [
+    {
+      bri: 209,
+			hue: 49767,
+			sat: 221,
+    },
+    {
+      bri: 209,
+			hue: 46764,
+			sat: 194,
+    },
+    {
+      bri: 209,
+			hue: 52403,
+			sat: 228,
     },
   ],
-  SUNSET: [
+  dawn: [
+    {
+      bri: 209,
+      hue: 56405,
+      sat: 241,
+    },
+    {
+      bri: 209,
+			hue: 64723,
+			sat: 240,
+    },
+    {
+      bri: 209,
+			hue: 59571,
+			sat: 219,
+    },
+  ],
+  sunrise: [
+    {
+      bri: 209,
+      hue: 65060,
+      sat: 226,
+    },
+    {
+      bri: 209,
+			hue: 4682,
+			sat: 214,
+    },
+    {
+      bri: 209,
+			hue: 61981,
+			sat: 246,
+    },
+  ],
+  sunriseEnd: [
+    {
+      bri: 209,
+			hue: 4672,
+			sat: 195,
+    },
+    {
+      bri: 209,
+			hue: 10503,
+			sat: 249,
+    },
+    {
+      bri: 209,
+			hue: 7360,
+			sat: 254,
+    },
+  ],
+  goldenHourEnd: [
+    {
+      bri: 169,
+      hue: 9079,
+      sat: 123,
+    },
+  ],
+  solarNoon: [
+    {
+      bri: 254,
+			hue: 40537,
+			sat: 42,
+    },
+  ],
+  goldenHour: [
+    {
+      bri: 169,
+      hue: 9079,
+      sat: 123,
+    },
+  ],
+  sunsetStart: [
     {
       bri: 103,
 			hue: 3951,
@@ -30,7 +172,72 @@ module.exports = {
 			sat: 254,
     },
   ],
-  DUSK: [],
-  NAUTICAL_DUSK: [],
-  NIGHT: [],
+  sunset: [
+    {
+      bri: 169,
+      hue: 63991,
+      sat: 254,
+    },
+    {
+      bri: 169,
+      hue: 2302,
+      sat: 247,
+    },
+    {
+      bri: 169,
+      hue: 4484,
+      sat: 254,
+    }
+  ],
+  dusk: [
+    {
+      bri: 169,
+			hue: 59453,
+			sat: 252,
+    },
+    {
+      bri: 169,
+			hue: 46835,
+			sat: 221,
+    },
+    {
+      bri: 169,
+			hue: 65114,
+			sat: 237,
+    },
+  ],
+  nauticalDusk: [
+    {
+      bri: 169,
+      hue: 46260,
+      sat: 238,
+    },
+    {
+      bri: 169,
+      hue: 45027,
+      sat: 254,
+    },
+    {
+      bri: 169,
+      hue: 48018,
+      sat: 216,
+    },
+  ],
+  night: [
+    {
+      bri: 169,
+			hue: 45996,
+			sat: 254,
+    },
+    {
+      bri: 169,
+			hue: 43724,
+			sat: 254,
+    },
+    {
+      bri: 169,
+			hue: 41909,
+			sat: 254,
+    }
+  ],
 }
